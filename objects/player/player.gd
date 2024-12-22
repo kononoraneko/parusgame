28ed21383017166193cbbf9d8f1744bdedf01d3b
noraneko <masima206@gmail.com>
2024-12-22 17:39:39 +0900

    added player textures

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -4,25 +4,17 @@ extends CharacterBody2D
 @export var speed = 300.0
 const JUMP_VELOCITY = -400.0
 
-func printHello() -> void:
-	print("Hello, world!")
-
+func _ready() -> void:
+	$AnimatedSprite2D.play("idle")
 
 func _process(delta: float) -> void:
 	movement_handler(delta)
 
 
 
-func _physics_process(delta: float) -> void:
-	pass
-	
-
-
-
-
 func movement_handler(delta: float) -> void:
-	var velocity = Vector2.ZERO
 	
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
@@ -34,8 +26,17 @@ func movement_handler(delta: float) -> void:
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+		
 	
-	position += velocity * delta 
+	if velocity.y != 0:
+		if velocity.y > 0:
+			$AnimatedSprite2D.animation = "walk_down"
+		if velocity.y < 0:
+			$AnimatedSprite2D.animation = "walk_up"
+	elif velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	
+	move_and_slide()
